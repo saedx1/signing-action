e70340e79df0758a0acb51ce8c76f600f271de26
saedx1 <saadmtsa@gmail.com>
2021-03-30 23:32:07 -0400

    My first action is ready

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,14 @@ FROM alpine:3.13.3
 
 RUN apk add autoconf automake libtool make build-base gcc abuild binutils binutils-doc gcc-doc openssl-dev curl-dev git
 
+RUN apk add --no-cache \
+        python3 \
+        py3-pip \
+    && pip3 install --upgrade pip \
+    && pip3 install \
+        awscli \
+    && rm -rf /var/cache/apk/*
+    
 WORKDIR /
 RUN git clone https://github.com/mtrojnar/osslsigncode.git
 WORKDIR /osslsigncode
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
+aws s3 cp $1 cert.pfx
 
-openssl pkcs12 -in $1  -password pass:$4 -nocerts -nodes -out key.pem
-openssl pkcs12 -in $1  -password pass:$4 -nokeys -nodes -out cert.pem
+openssl pkcs12 -in cert.pfx  -password pass:$4 -nocerts -nodes -out key.pem
+openssl pkcs12 -in cert.pfx  -password pass:$4 -nokeys -nodes -out cert.pem
 openssl rsa -in key.pem -outform DER -out authenticode.key
 openssl crl2pkcs7 -nocrl -certfile cert.pem -outform DER -out authenticode.spc
 
